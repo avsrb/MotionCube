b74a7502d1079ad5f10b422adae45e5853089c85
Artem <Serebryakov>
2022-08-19 09:12:48 +0300

    Initial Commit

---
+++ MotionCude/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MotionCude
+//
+//  Created by Artem Serebriakov on 19.08.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
